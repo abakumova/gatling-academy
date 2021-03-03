ddc489244b2d79b9aaebee820b76063f1f181a65
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2021-03-03 16:42:26 +0200

    A4 Record User Journey

--- src/test/scala/Recorder.scala
+++ src/test/scala/Recorder.scala
@@ -6,7 +6,7 @@ object Recorder extends App {
 	val props = new RecorderPropertiesBuilder()
 		.simulationsFolder(IDEPathHelper.mavenSourcesDirectory.toString)
 		.resourcesFolder(IDEPathHelper.mavenResourcesDirectory.toString)
-		.simulationPackage("computerdatabase")
+		.simulationPackage("demostore")
 
 	GatlingRecorder.fromMap(props.build, Some(IDEPathHelper.recorderConfigFile))
 }
