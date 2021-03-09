7e4c22bce0c36e9988aeb52ea3e6352c05a47cea
Viktoriia <34584226+abakumova@users.noreply.github.com>
2021-03-09 16:29:51 +0200

    Section B Gatling Scripting
    * B2 Refactor Home & About Pages
    
    * B3 Refactor Category
    
    * B4 Refactor Product
    
    * B5 Refactor Add Product
    
    * B6 Refactor View Cart
    
    * B7 Refactor Login
    
    * B8 Complete Checkout
    
    * B9 Init Session
    
    * B10 Login Object Refactor
    
    * B11 Cart Total

--- src/test/scala/demostore/DemostoreSimulation.scala
+++ src/test/scala/demostore/DemostoreSimulation.scala
@@ -3,6 +3,8 @@ package demostore
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.util.Random
+
 class DemostoreSimulation extends Simulation {
 
   val domain = "demostore.gatling.io"
@@ -10,35 +12,140 @@ class DemostoreSimulation extends Simulation {
   val httpProtocol = http
     .baseUrl("http://" + domain)
 
+  val categoryFeeder = csv("data/categoryDetails.csv").random
+  val productFeeder = jsonFile("data/productDetails.json").random
+  val loginFeeder = csv("data/loginDetails.csv").circular
+
+  val rnd = new Random()
+
+  def randomString(length: Int): String = {
+    rnd.alphanumeric.filter(_.isLetter).take(length).mkString
+  }
+
+  val initSession = exec(flushCookieJar)
+    .exec(session => session.set("randomNumber", rnd.nextInt))
+    .exec(session => session.set("customerLoggedIn", false))
+    .exec(session => session.set("cartTotal", 0.00))
+    .exec(addCookie(Cookie("sessionId", randomString(10)).withDomain(domain)))
+    .exec { session => println(session); session }
+
+  object CmsPages {
+    def homePage = {
+      exec(http("Load Home page")
+        .get("/")
+        .check(status.is(200))
+        .check(regex("<title>Gatling Demo-Store</title>").exists)
+        .check(css("#_csrf", "content").saveAs("csrfValue")))
+    }
+
+    def aboutUsPage = {
+      exec(http("Load About us page")
+        .get("/about-us")
+        .check(status.is(200))
+        .check(substring("About Us")))
+    }
+  }
+
+  object Catalog {
+
+    object Category {
+      def view = {
+        feed(categoryFeeder)
+          .exec(http("Load Category page - ${categoryName}")
+            .get("/category/${categorySlug}")
+            .check(status.is(200))
+            .check(css("#CategoryName").is("${categoryName}")))
+      }
+    }
+
+    object Product {
+      def view = {
+        feed(productFeeder)
+          .exec(http("Load Product page - ${name}")
+            .get("/product/${slug}")
+            .check(status.is(200))
+            .check(css("#ProductDescription").is("${description}"))
+          )
+      }
+
+      def add = {
+        exec(view).
+          exec(http("Add Product to Cart")
+            .get("/cart/add/${id}")
+            .check(status.is(200))
+            .check(substring("items in your cart"))
+          )
+          .exec(session => {
+            val currentCartTotal = session("cartTotal").as[Double]
+            val itemPrice = session("price").as[Double]
+            session.set("cartTotal", (currentCartTotal + itemPrice))
+          })
+      }
+    }
+
+  }
+
+  object Checkout {
+
+    def viewCart = {
+      doIf(session => !session("customerLoggedIn").as[Boolean]) {
+        exec(Customer.login)
+      }
+        .exec(
+          http("Load Cart Page")
+            .get("/cart/view")
+            .check(status.is(200))
+            .check(css("#grandTotal").is("$$${cartTotal}"))
+        )
+    }
+
+    def completeCheckout = {
+      exec(
+        http("Checkout Cart")
+          .get("/cart/checkout")
+          .check(status.is(200))
+          .check(substring("Thanks for your order! See you soon!"))
+      )
+    }
+  }
+
+  object Customer {
+
+    def login = {
+      feed(loginFeeder)
+        .exec(
+          http("Load Login Page")
+            .get("/login")
+            .check(status.is(200))
+            .check(substring("Username:"))
+        )
+        .exec { session => println(session); session }
+        .exec(
+          http("Customer Login Action")
+            .post("/login")
+            .formParam("_csrf", "${csrfValue}")
+            .formParam("username", "${username}")
+            .formParam("password", "${password}")
+            .check(status.is(200))
+        )
+        .exec(session => session.set("customerLoggedIn", true))
+        .exec { session => println(session); session }
+    }
+  }
+
   val scn = scenario("RecordedSimulation")
-    .exec(http("Load Home page")
-      .get("/")
-      .check(regex("<title>Gatling Demo-Store</title>").exists)
-      .check(css("#_csrf", "content").saveAs("csrfValue")))
+    .exec(initSession)
+    .exec(CmsPages.homePage)
     .pause(2)
-    .exec(http("Load About us page")
-      .get("/about-us"))
+    .exec(CmsPages.aboutUsPage)
     .pause(2)
-    .exec(http("Load Category page")
-      .get("/category/all"))
+    .exec(Catalog.Category.view)
     .pause(2)
-    .exec(http("Load Product page")
-      .get("/product/black-and-red-glasses"))
+    .exec(Catalog.Product.add)
     .pause(2)
-    .exec(http("Add product to the cart")
-      .get("/cart/add/19"))
+    .exec(Checkout.viewCart)
     .pause(2)
-    .exec(http("View cart")
-      .get("/cart/view"))
-    .pause(2)
-    .exec(http("Log in")
-      .post("/login")
-      .formParam("_csrf", "${csrfValue}")
-      .formParam("username", "user1")
-      .formParam("password", "pass"))
-    .pause(2)
-    .exec(http("Checkout")
-      .get("/cart/checkout"))
+    .exec(Checkout.completeCheckout)
 
   setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
